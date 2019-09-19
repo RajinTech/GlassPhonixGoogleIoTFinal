689f5591570c0bf8a402c82b2c66d555fb6c150d
RajinTech <rajsglass@gmail.com>
2019-09-19 14:48:50 -0400

    added other pins

--- charizard_mqttScript_GPFinal/local_test_node.js
+++ charizard_mqttScript_GPFinal/local_test_node.js
@@ -1,11 +1,17 @@
 const Gpio = require('pigpio').Gpio;
 
-const led = new Gpio(27, {mode: Gpio.OUTPUT});
+const insidePropane = new Gpio(27, {mode: Gpio.OUTPUT});
+const insideOxygen = new Gpio(25, {mode: Gpio.OUTPUT});
+const outsidePropane = new Gpio(23, {mode: Gpio.OUTPUT});
+const outsideOxygen = new Gpio(24, {mode: Gpio.OUTPUT});
 
 let dutyCycle = 0;
 
 setInterval(() => {
-  led.pwmWrite(dutyCycle);
+  insidePropane.pwmWrite(dutyCycle);
+  insideOxygen.pwmWrite(dutyCycle);
+  outsidePropane.pwmWrite(dutyCycle);
+  outsideOxygen.pwmWrite(dutyCycle);
 
   dutyCycle += 5;
   if (dutyCycle > 255) {
