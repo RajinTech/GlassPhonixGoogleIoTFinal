9a265cec4bcc55c627c234a56e30829503e1931b
RajinTech <rajsglass@gmail.com>
2019-09-19 16:25:06 -0400

    added view

--- charizard_mqttScript_GPFinal/charizard_listener.js
+++ charizard_mqttScript_GPFinal/charizard_listener.js
@@ -23,7 +23,7 @@ const gpio = require('onoff').Gpio;
 const sleep = require('sleep');
 const {PubSub} = require('@google-cloud/pubsub');
 //pi
-const Gpio = require('pigpio').Gpio;
+//const Gpio = require('pigpio').Gpio;
 const outsidePropane = new gpio(23, 'out');
 const outsideOxygen = new gpio(24, 'out');
 const insideOxygen = new gpio(25, 'out');
