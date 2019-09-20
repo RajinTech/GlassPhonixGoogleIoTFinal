baa525b5efdf15ea317c0819c65435504eab078e
RajinTech <rajsglass@gmail.com>
2019-09-20 16:43:52 -0400

    added rpio-gpio

--- charizard_mqttScript_GPFinal/blink_test_rpi-gpio_lib.js
+++ charizard_mqttScript_GPFinal/blink_test_rpi-gpio_lib.js
@@ -1,9 +1,9 @@
 var gpio = require('rpi-gpio')
 var gpiop = gpio.promise;
 
-gpiop.setup(7, gpio.DIR_OUT)
+gpiop.setup(27, gpio.DIR_OUT)
     .then(() => {
-        return gpiop.write(7, true)
+        return gpiop.write(27, true)
     })
     .catch((err) => {
         console.log('Error: ', err.toString())
