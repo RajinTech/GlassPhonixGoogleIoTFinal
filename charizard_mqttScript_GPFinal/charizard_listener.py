3dd7e13ce01891fcfed47717a58aa7a7ae2b2c47
RajinTech <rajsglass@gmail.com>
2019-09-21 14:59:09 -0400

    added python libraries

--- charizard_mqttScript_GPFinal/charizard_listener.py
+++ charizard_mqttScript_GPFinal/charizard_listener.py
@@ -134,6 +134,14 @@ subscription_path = subscriber.subscription_path(
     project_id, subscription_name)
 
 def callback(message):
+    flame = {
+    'IP': '100',
+    'IO': '100',
+    'OP': '100',
+    'OO': '100',
+    }
+
+
 
     mirage = Torch()
 
@@ -142,8 +150,11 @@ def callback(message):
     #print('Received message1: {}'.format(message))
     print("New Cycle")
     print('Received message2: {}'.format(message.data).split('@')[1])
-    flame = ast.listeral_eval(message.data.split('@')[1])
-    print('flameIP', flame['IP'])
+    flameSettings = ast.listeral_eval(message.data.split('@')[1])
+    flame['IP'] = flameSettings['IP']
+    flame['IO'] = flameSettings['IO']
+    flame['OP'] = flameSettings['OP']
+    flame['OO'] = flameSettings['OO']
 
     message.ack()
 
