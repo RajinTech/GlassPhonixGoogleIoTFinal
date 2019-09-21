6628a0c6188966aaafe265e3e79816643c9b944e
RajinTech <rajsglass@gmail.com>
2019-09-20 20:03:34 -0400

    node

--- charizard_mqttScript_GPFinal/charizard_listener_onoff_sync.js
+++ charizard_mqttScript_GPFinal/charizard_listener_onoff_sync.js
@@ -69,14 +69,13 @@ function charizardListener(
   let messageCount = 0;
   const messageHandler = message => {
 
-    //insidePropanecommand = parseFloat(message.data.toString().substr(32, 2))/10;
-    insideOxygencommand = parseFloat(message.data.toString().substr(37, 2))/10;
-    outsidePropanecommand = parseFloat(message.data.toString().substr(42, 2))/10;
-    outsideOxygencommand = parseFloat(message.data.toString().substr(47, 2))/10;
+
 
     let preData = JSON.parse(message.data.toString().split('@')[1])
-    insidePropanecommand = preData.IP
-    console.log(insidePropanecommand)
+    insidePropanecommand = preData.IP/10
+    insideOxygencommand = preData.IO/10
+    outsidePropanecommand = preData.OP/10
+    outsideOxygencommand = preData.OO/10
 
 
 
